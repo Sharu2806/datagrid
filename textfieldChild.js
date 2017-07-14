829f9ec580374fa59628f277424b961713316368
Sharu2806 <jagtap.sharayu03@yahoo.com>
2017-07-14 19:48:58 +0530

    Update textfieldChild.js

--- textfieldChild.js
+++ textfieldChild.js
@@ -5,18 +5,26 @@ export default class SerialNoTextFields extends Component{
 
 
   render(){
-      let elem = "";
+    return(
+      <tr>
+          <th scope="row" id={this.props.rowID}>{this.props.rowTitle}</th>
+        <td>
+            <input type="number"  id={this.props.minID} name={this.props.nameForMinval} className="textfieldsmall" max="6" onkeypress={this.props.onkeypress} onChange={this.props.handleChange} value={this.props.minValue}/>
+            <span name={this.props.minID} className="hideError"></span>
+        </td>
+        <td>
+            <input type="number"   id={this.props.maxID} name={this.props.nameForMaxval} className="textfieldsmall" max="6" onChange={this.props.handleChange} value={this.props.maxValue}/>
+            <span name={this.props.maxID} className="hideError"></span>
+        </td>
+        <td>
+            <input type="number"  id={this.props.allocID} name={this.props.nameForAllocNo} className="textfieldsmall" max="6" onChange={this.props.handleChange} value={this.props.allocNo}/>
+            <span name={this.props.allocID} className="hideError"></span>
+        </td>
+      </tr>
 
-      if(this.props.nameForMinval == 'min_val'){
-        elem = <td><input id={id} type="text" name={this.props.nameForMinval} className="textfieldsmall" maxLength="6" onChange={this.props.handleChangeMin} value={this.props.minValue}/></td>
-      }
-      if(this.props.nameForMaxval == 'max_val'){
-        elem = <td><input id={id} type="text" name={this.props.nameForMaxval} className="textfieldsmall" maxLength="6" onChange={this.props.handleChangeMax} value={this.props.maxValue}/></td>
-      }
-      if(this.props.nameForAllocNo == 'alloc_no'){
-        elem =<td><input id={id} type="text" name={this.props.nameForAllocNo} className="textfieldsmall" maxLength="6" onChange={this.props.handleChangeOnAlloc} value={this.props.allocNo}/></td>
-      }
+    );
 
-    return elem;
+
+
   }
 }
